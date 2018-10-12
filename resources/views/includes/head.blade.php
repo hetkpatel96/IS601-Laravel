9ca411110bd45f3b543cd4c551f83b02c3845423
Het Patel <hp428@njit.edu>
2018-10-12 09:40:55 -0400

    Edit

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="Learana Learning Analytics">
-<meta name="author" content="keith">
+<meta name="author" content="Het">
 
 <title>Learana - Learning Analytics</title>
 
